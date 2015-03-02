ea0dc22e8f30e90d758b3a944a47aebfee15e15c
Efi Cohen <efi@datorama.com>
2015-03-02 15:53:30 +0200

    fix an issue with the timezone

--- rds_stats.py
+++ rds_stats.py
@@ -1,21 +1,7 @@
-#!/usr/bin/env python
-
-# script that monitors CloudWatch stats for RDS instances.
-#
-# Requirements:
-# - Boto (http://boto.s3.amazonaws.com/index.html)
-#
-# Plugin arguments:
-# 1. DBInstanceIdentifier (name)
-# 2. AWS Access Key
-# 3. AWS Secret Access Key
-# 4. RDS Metric
-#
-
 import datetime
 import sys
 from optparse import OptionParser
-from boto.ec2.cloudwatch import CloudWatchConnection
+import boto.ec2.cloudwatch
 
 ### Arguments
 parser = OptionParser()
@@ -27,6 +13,8 @@ parser.add_option("-k", "--secret-key", dest="secret_key",
                 help="AWS Secret Access Key")
 parser.add_option("-m", "--metric", dest="metric",
                 help="RDS cloudwatch metric")
+parser.add_option("-r", "--region", dest="region",
+                help="RDS region")
 
 (options, args) = parser.parse_args()
 
@@ -40,7 +28,7 @@ if (options.metric == None):
     parser.error("-m RDS cloudwatch metric is required")
 ###
 
-### Actual code
+### Real code
 metrics = {"CPUUtilization":{"type":"float", "value":None},
     "ReadLatency":{"type":"float", "value":None},
     "DatabaseConnections":{"type":"int", "value":None},
@@ -52,15 +40,26 @@ metrics = {"CPUUtilization":{"type":"float", "value":None},
     "SwapUsage":{"type":"float", "value":None},
     "ReadThroughput":{"type":"float", "value":None},
     "DiskQueueDepth":{"type":"float", "value":None},
+    "ReplicaLag":{"type":"int", "value":None},
+    "DiskQueueDepth":{"type":"float", "value":None},
+    "ReplicaLag":{"type":"int", "value":None},
     "NetworkReceiveThroughput":{"type":"float", "value":None},
     "NetworkTransmitThroughput":{"type":"float", "value":None},
-    "ReplicaLag":{"type":"int", "value":None},
     "FreeStorageSpace":{"type":"float", "value":None}}
-
-end = datetime.datetime.now()
+end = datetime.datetime.utcnow()
 start = end - datetime.timedelta(minutes=5)
 
-conn = CloudWatchConnection(options.access_key, options.secret_key)
+#get the region
+if (options.region == None):
+    options.region = 'us-east-1'
+    
+for r in boto.ec2.cloudwatch.regions():
+   if (r.name == options.region):
+      region = r
+      break
+
+conn = boto.ec2.cloudwatch.CloudWatchConnection(options.access_key, options.secret_key,region=region)
+
 for k,vh in metrics.items():
 
     if (k == options.metric):
@@ -68,7 +67,7 @@ for k,vh in metrics.items():
         try:
                 res = conn.get_metric_statistics(60, start, end, k, "AWS/RDS", "Average", {"DBInstanceIdentifier": options.instance_id})
         except Exception, e:
-                print "Error executing rds_stats: %s" % e.error_message
+                print "status err Error running rds_stats: %s" % e.error_message
                 sys.exit(1)
         average = res[-1]["Average"] # last item in result set
         if (k == "FreeStorageSpace" or k == "FreeableMemory"):
@@ -78,5 +77,6 @@ for k,vh in metrics.items():
         if vh["type"] == "int":
                 metrics[k]["value"] = "%i" % average
 
+        #print "metric %s %s %s" % (k, vh["type"], vh["value"])
         print "%s" % (vh["value"])
         break
